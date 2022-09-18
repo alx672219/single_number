060d1d5e0bfb375444599bd4368dce6b78bf8d6d
Alex <113051612+alx672219@users.noreply.github.com>
2022-09-18 11:30:20 -0400

    Add files via upload

---
+++ single_number.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        no_duplicate_list = []
+        for i in nums:
+            if i not in no_duplicate_list:
+                no_duplicate_list.append(i)
+            else:
+                no_duplicate_list.remove(i)
+        return no_duplicate_list.pop()   # if I do not do pop, then [n] instead of n
